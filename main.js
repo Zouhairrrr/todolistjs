5f62be973c61c9dc4a405e0d2c84edb23787dc5e
Zouhairrrr <zouhair.ouknaz@gmail.com>
2021-12-03 17:18:49 +0100

    commit - first structure

---
+++ main.js
@@ -0,0 +1,40 @@
+// definition des variable utilisés ( inputs buttons ...)
+
+
+/*
+    ## Exemples: 
+let x = document.getElementById('btn');
+
+*/
+
+
+
+
+
+
+
+
+// definition of class used [ 'User , 'Tasks', 'Project']
+class Tasks {
+
+    // definition of the attribues 
+    // name = this.name;
+
+
+
+
+}
+class User {
+    // definition of the attribues 
+
+
+
+}
+class Project {
+    // definition of the attribues 
+
+
+
+
+
+}
