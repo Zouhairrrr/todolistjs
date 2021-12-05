8d53d0be6dddb1732af97cc2e20d094ef160996e
Zouhairrrr <zouhair.ouknaz@gmail.com>
2021-12-05 22:36:10 +0100

    commit

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+/*
+    diffrence beetwen Class & function : 
+        ## functions are hoisted Class is not. Very important !
+        ## functions can be overwriten unlike Class you can't 
+
+*/
 // definition des variable utilisés ( inputs buttons ...)
+let HomeInputs = document.querySelector("input");
+let submitHomeButton = document.getElementById("btn-home")
 
 
 /*
@@ -9,27 +17,45 @@ let x = document.getElementById('btn');
 
 
 
-
-
-
-
-
 // definition of class used [ 'User , 'Tasks', 'Project']
 class Tasks {
 
-    // definition of the attribues 
-    // name = this.name;
+    constructor(nameTask, numberOfTasks) {
+        this.nameTask = nameTask;
+        this.numberOfTasks = numberOfTasks;
 
+    }
 
+    Task(el = this.nameTask) {
+        return "bruh this is " + `${el}`;
+    }
 
 
 }
+let m = new Tasks();
+console.log(m.Task('Jhon'))
+
+
+
+
+
 class User {
-    // definition of the attribues 
+    constructor(name) {
+        this.name = name
+    }
+    ShowName(el = this.name) {
 
-
-
+        return "hello its me " + `${el}`;
+    }
 }
+
+
+let d = new User();
+
+console.log(d.ShowName('Jhon'))
+
+
+
 class Project {
     // definition of the attribues 
 
